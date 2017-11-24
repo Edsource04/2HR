96285cdef90063e3543dde1a826aa9e35a5e54dc
Edsource04 <ing_edgar_rivera@hotmail.com>
2017-11-24 11:39:40 -0500

    Update admin-routing.modules.ts
    add job-application-form-view routing

--- src/app/modules/admin/admin-routing.modules.ts
+++ src/app/modules/admin/admin-routing.modules.ts
@@ -1,10 +1,15 @@
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobApplicationFormView } from './../information/components/job-application-form-view/job-application-form-view.ts';
 
 const routes: Routes = [
   {
-    path: 'admin-dashboard', component: AdminDashboardComponent, pathMatch: 'full'
+    path: 'admin-dashboard', component: AdminDashboardComponent, pathMatch: 'full', children: [
+      {
+        path: 'application-form-view', component: JobApplicationFormView, router: 'view'
+      }
+    ]
   }
 ];
 
