758dbb6cc4ed5750ab3349f0e41a0035796b5a05
Edsource04 <ing_edgar_rivera@hotmail.com>
2017-11-24 11:31:23 -0500

    Create job-application-form-view.ts
    job-application-form-view component created

---
+++ src/app/modules/information/components/job-application-form-view/job-application-form-view.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Application, 
+         WorkDays, 
+         FamilyMember, 
+         EducationApplication, 
+         WorkExpApplication, 
+         SkillApplication, 
+         LanguageApplication, 
+         PersonalReference } from '../../models/application.ts';
+
+@Component({
+   selector: 'hr-job-application-form-view',
+   templateUrl: './job-application-form-view.html';
+   styleUrls: ['./job-application-form-view.css']
+})
+
+export class JobApplicationFormView implements OnInit{
+
+   constructor(){}
+   
+   ngOnInit(){
+   
+   }
+}
