22946f62def86f33df9b14e07256c66dcf6c698a
Edsource04 <ing_edgar_rivera@hotmail.com>
2017-11-29 13:17:14 -0500

    Create application-list.component.ts

---
+++ src/app/modules/admin/components/application-list/application-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core;
+import { Application } from './../../../information/models/application';
+import { AdminService } from './../../admin.service';
+import { Router } from '@angular/router';
+
+@Component({
+   selector: 'hr-application-list',
+   templateUrl: './application-list.component.html',
+   styleUrls: ['./application-list.component.css']
+})
+
+export class ApplicationListComponent implements OnInit{
+   applications: Array<Application>;
+   
+   constructor(private adminService: AdminService, private router: Router){}
+   
+   ngOnInit(){
+      this.adminService.getApplications().subscribe(app => this.applications = app);
+   }
+   
+   viewApplication(id: string){
+      this.router.navigate(['{'admin': ['applications', '+id+']}}]" ']);
+   }
+}
