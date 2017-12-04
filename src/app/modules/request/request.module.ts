0d1df590854ec0982f438eca9c961cd8bdd9349a
edgar rivera <edgar rivera>
2017-12-03 22:42:46 -0500

    move Components

--- src/app/modules/request/request.module.ts
+++ src/app/modules/request/request.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { RequestRoutingModule } from './request-routing.module';
 import { RequestComponent } from './components/request/request.component';
-import { RequestFormComponent } from './component/request-form/request-form.component';
-import { SendFormComponent } from './component/send-form/send-form.component';
+import { RequestFormComponent } from './components/request-form/request-form.component';
+import { SendFormComponent } from './components/send-form/send-form.component';
 
 @NgModule({
   imports: [
