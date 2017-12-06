2243ffd9581247b1c97de28a26db84ede4fdc062
Edsource04 <ing_edgar_rivera@hotmail.com>
2017-12-06 11:08:16 -0500

    Update complains.component.ts
    adding properties

--- src/app/modules/request/components/complains/complains.component.ts
+++ src/app/modules/request/components/complains/complains.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complains.component.css']
 })
 export class ComplainsComponent implements OnInit {
-
+  
+  header: string;
+  atDate: Date = new Date();
+  description: string;
+  submittedBy: string;
+  
   constructor() { }
 
   ngOnInit() {
+     var actualUser = JSON.parse(localStorage.getItem('actualUser'));
+     this.submittedBy = actualUser.user.username;
+  }
+  
+  sendComplain(){
+  
   }
 
 }
