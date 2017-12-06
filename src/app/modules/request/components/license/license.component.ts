adeae2694d25e840b4b1750cd5e853fa382f62a0
Edsource04 <ing_edgar_rivera@hotmail.com>
2017-12-06 11:18:37 -0500

    Update license.component.ts
    adding properties to license component

--- src/app/modules/request/components/license/license.component.ts
+++ src/app/modules/request/components/license/license.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./license.component.css']
 })
 export class LicenseComponent implements OnInit {
-
+  
+  empNumber: string;
+  submittedBy: string;
+  fullname: string;
+  manager: string;
+  department: string;
+  attachment: File;
+  description: string;
+  atDate: Date = new Date();
+  
   constructor() { }
 
   ngOnInit() {
+    var actualUser = JSON.parse(localStorage.getItem('actualUser'));
+    this.submittedBy = actualUser.user.username;
   }
-
+ 
+  sendLicense(){
+  
+  }
 }
